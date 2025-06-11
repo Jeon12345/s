f1ddc53a8be719f98f928383f46777e52403911d
Jeon12345 <53422713+Jeon12345@users.noreply.github.com>
2025-06-11 17:54:31 +0600

    Update n.sh

--- n.sh
+++ n.sh
@@ -4,18 +4,19 @@
 NGROK_TOKEN="2yMCukUmuN2VdA6ZsOR5NrZJJSu_6C8UNqAT2QDxSqjiuLM1U"
 
 echo "[+] Installing ngrok..."
+
 # Download and install ngrok if not found
 if ! command -v ngrok &> /dev/null; then
   wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-  unzip ngrok-v3-stable-linux-amd64.tgz
-  sudo mv ngrok /usr/local/bin
+  tar -xzf ngrok-v3-stable-linux-amd64.tgz
+  sudo mv ngrok /usr/local/bin/
   rm ngrok-v3-stable-linux-amd64.tgz
 else
   echo "[+] ngrok is already installed."
 fi
 
 echo "[+] Setting authtoken..."
-ngrok config add-authtoken $NGROK_TOKEN
+ngrok config add-authtoken "$NGROK_TOKEN"
 
 echo "[+] Starting ngrok tunnel on port 22..."
-# Run in background
+ngrok tcp 22

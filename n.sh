58027e8fe450d5e0a19eed3a690ba1b4e18b821b
Jeon12345 <53422713+Jeon12345@users.noreply.github.com>
2025-07-10 21:00:24 +0600

    Update n.sh

--- n.sh
+++ n.sh
@@ -2,7 +2,7 @@
 
 # Set your Ngrok authtoken here
 # NGROK_TOKEN="1pEQhvizMarEn9dwdgrTw6imo07_3dAuarJwJNztGS4BqbueD"
-NGROK_TOKEN="2nLS3Bc3UzGKpvvfaPrtcTQxMb5_2foCs3kEMA3x64ewRY6P5"
+NGROK_TOKEN="2EIiWgE6kbeXb2b3S88gJoeatzG_7KFcUJPNbwK7CHwHoucmY"
 
 echo "[+] Installing ngrok..."
 
